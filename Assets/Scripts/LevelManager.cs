41d76cc46ff59157c36733cc295bf2d3eeb95d6c
Daniel <15daniel94@gmail.com>
2019-12-13 10:16:41 +1030

    Added infinite scrolling on the X axis, platforms are pooled and moved back to the front when they move off the back of the screen.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private float score;
+
+    private ObjectPooler objectPooler;
+
+    private List<GameObject> platformList;
+    private Vector3 platformSpawnLocation;
+
+    private float deltaX;
+    private float platformLength;
+
+
+    [SerializeField] private float horizontalMoveSpeed;
+    [SerializeField] private int maxActivePlatforms;
+    [SerializeField] private int numberOfInitialPlatforms;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.SharedInstance;
+        platformList = new List<GameObject>();
+        platformSpawnLocation = new Vector3(0, -2, 0);
+        deltaX = 0;
+
+        GameObject initialPlatform = objectPooler.GetObjectFromPool();
+        initialPlatform.transform.position = platformSpawnLocation;
+        initialPlatform.SetActive(true);
+        //platformLength = initalPlatform.GetComponent<Collider2D>().bounds.size.x;
+        platformLength = initialPlatform.transform.localScale.x;
+
+        platformList.Add(initialPlatform);
+
+        for (int i = 1; i < numberOfInitialPlatforms; i++)
+        {
+            initialPlatform = objectPooler.GetObjectFromPool();
+            initialPlatform.transform.position = platformSpawnLocation + Vector3.right * i * platformLength;
+            initialPlatform.SetActive(true);
+            platformList.Add(initialPlatform);
+        }
+    }
+
+    private void Update()
+    {
+
+        deltaX += horizontalMoveSpeed * Time.deltaTime;
+        score += horizontalMoveSpeed * Time.deltaTime;
+        Debug.Log(score);
+
+        if (deltaX >= platformLength)
+        {
+            float excess = deltaX - platformLength;
+
+            GameObject newPlatform = objectPooler.GetObjectFromPool();
+            //newPlatform.transform.position = platformSpawnLocation + Vector3.right * platformLength * (numberOfInitialPlatforms - 1);
+            newPlatform.transform.position = platformList[platformList.Count - 1].transform.position + (Vector3.right * (platformLength + excess));
+            newPlatform.SetActive(true);
+            platformList.Add(newPlatform);
+            deltaX = 0;
+        }
+
+        if (platformList.Count > maxActivePlatforms)
+        {
+            platformList[0].SetActive(false);
+            platformList.RemoveAt(0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < platformList.Count; i++)
+        {
+            platformList[i].transform.Translate(new Vector3(-horizontalMoveSpeed * Time.deltaTime, 0, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler SharedInstance;
+
+    [SerializeField]
+    private GameObject objectToPool;
+    [SerializeField]
+    private int amountToPool = 5;
+
+    private List<GameObject> pool;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    private void Start()
+    {
+        if (pool == null)
+        {
+            GeneratePool();
+        }
+    }
+
+    public GameObject GetObjectFromPool()
+    {
+        if (pool == null)
+        {
+            GeneratePool();
+        }
+        for (int i = 0; i < pool.Count; i++)
+        {
+            if (!pool[i].activeInHierarchy)
+            {
+                return pool[i];
+            }
+        }
+        return null;
+    }
+
+    private void GeneratePool()
+    {
+        pool = new List<GameObject>();
+
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject o = Instantiate(objectToPool);
+            o.SetActive(false);
+            pool.Add(o);
+        }
+    }
+}
